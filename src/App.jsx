f42d34d556f26b2fd394c94d025cffbce9158376
Justin Lim <justinlimchunkiat.work@gmail.com>
2024-07-07 15:11:45 +0800

    Initial justin commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,27 @@
 import "./App.css";
-import { styled } from "@mui/material/styles";
-import { Grid, Box, Paper } from "@mui/material";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import SideMenu from "./components/SideMenu/SideMenu.jsx";
-import TopBar from "./components/TopBar/TopBar.jsx";
-import AssetDashboard from "./content/AssetDashboard.jsx";
+import MainPage from "./pages/Main/MainPage.jsx";
+import LoginPage from "./pages/Login/LoginPage.jsx";
+import NotFoundPage from "./pages/Error/ErrorPage.jsx";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/main",
+    element: <MainPage />,
+  },
+]);
 
 function App() {
   return (
-    <Box sx={{ height: "100vh", p: '0 16px 0 16px' }}>
-      <Grid container spacing={2} sx={{ height: "100%" }}>
-        {/* Top item */}
-        <Grid item xs={12} sx={{ height: "10%", p:0}}>
-          <TopBar></TopBar>
-        </Grid>
-        {/* Bottom items */}
-        <Grid container item xs={12} spacing={2} sx={{ height: "85%" }}>
-          <Grid item xs={2} sx={{ height: "100%" }}>
-            <SideMenu></SideMenu>
-          </Grid>
-          <Grid item xs={10} sx={{ height: "100%" }}>
-            <AssetDashboard></AssetDashboard>
-          </Grid>
-        </Grid>
-      </Grid>
-    </Box>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
