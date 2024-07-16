d0bc107362bf514494644c2e473ced684eddca80
Justin Lim <justinlimchunkiat.work@gmail.com>
2024-07-16 17:15:46 +0800

    Initial justin commit

--- src/components/AlarmCountSystem/AlarmSystemCount.jsx
+++ src/components/AlarmCountSystem/AlarmSystemCount.jsx
@@ -1,30 +1,26 @@
-import * as React from 'react';
-import { BarChart } from '@mui/x-charts/BarChart';
-import "../../content/AssetDashboard.css";
+import * as React from "react";
+import { BarChart } from "@mui/x-charts/BarChart";
+import "../../content/Asset/Dashboard/AssetDashboard.css";
 
 export default function AlarmSystemTrend() {
   const data = [
-    { system: 'System A', high: 4 },
-    { system: 'System B', high: 3 },
-    { system: 'System C', high: 5 },
-    { system: 'System D', high: 6 },
-    { system: 'System E', high: 8 },
-    { system: 'System F', high: 9 },
-    { system: 'System G', high: 7 },
-
+    { system: "System A", high: 4 },
+    { system: "System B", high: 3 },
+    { system: "System C", high: 5 },
+    { system: "System D", high: 6 },
+    { system: "System E", high: 8 },
+    { system: "System F", high: 9 },
+    { system: "System G", high: 7 },
   ];
 
   return (
     <div className="bar-chart-container">
       <BarChart
-        xAxis={[{ scaleType: 'band', data: data.map(item => item.system) }]}
-        series={[
-          { data: data.map(item => item.high), color: '#c3f833' },
-        ]}
+        xAxis={[{ scaleType: "band", data: data.map((item) => item.system) }]}
+        series={[{ data: data.map((item) => item.high), color: "#c3f833" }]}
         width={650}
         height={150}
       />
     </div>
   );
 }
-
