bc051054356b825f0d103250b99807e31d9835f2
jia jia <leejiaqian@Lees-MacBook-Pro-2.local>
2024-07-17 00:47:19 +0800

    Initial jiajia commit

--- src/main.jsx
+++ src/main.jsx
@@ -23,8 +23,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div>
-      <RouterProvider router={router} />
-    </div>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
