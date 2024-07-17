fd23d74cf864716809d48b7804bdaf5fe9b49f70
jia jia <leejiaqian@Lees-MacBook-Pro-2.local>
2024-07-18 02:10:50 +0800

    Initial jiajia commit

---
+++ src/components/PieChartMaintenance/PieChartMaintenance.jsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import {
+    PieChart,
+    Pie,
+    Cell,
+    BarChart,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Legend,
+    ResponsiveContainer,
+    LineChart,
+    Line,
+    Tooltip,
+} from "recharts";
+
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#8884d8", "#82ca9d", "#a4de6c", "#d0d1e6", "#f4cae4"];
+
+
+const data = [
+    { name: "Scheduled", value: 6 },
+    { name: "Cancelled", value: 0 },
+    { name: "OverDue", value: 0 },
+    { name: "Closed", value: 1 },
+    { name: "Started", value: 0 },
+    { name: "Completed", value: 0 },
+];
+
+
+export default function PieChartMaintenance() {
+    return (
+        <ResponsiveContainer width="100%" height="100%">
+            <PieChart>
+                <Pie data={data} cx="50%" cy="50%" outerRadius={60} fill="#8884d8">
+                    {data.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                </Pie>
+            </PieChart>
+        </ResponsiveContainer>
+
+    );
+}
--- src/content/Maintenance/Dashboard/MaintenanceDashboard.jsx
+++ src/content/Maintenance/Dashboard/MaintenanceDashboard.jsx
@@ -1,11 +1,92 @@
 import "./MaintenanceDashboard.css";
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import { Typography, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import {
+  PieChart,
+  Pie,
+  Cell,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  Tooltip,
+} from "recharts";
+import PieChartMaintenance from "../../../components/PieChartMaintenance/PieChartMaintenance";
+import BarChartMaintenanceOverall from "../../../components/BarChartMaintenanceOverall/BarChartMaintenanceOverall";
+import BarChartMaintenanceYearly from "../../../components/BarChartMaintenanceYearly/BarChartMaintenanceYearly";
+import BarChartMaintenanceEachSys from "../../../components/BarChartMaintenanceEachSys/BarChartMaintenanceEachSys";
 
-import { Typography } from "@mui/system";
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  height: "100%", // Ensure each item takes the full height of its container
+}));
+
+
+
+
+const barChartData = [
+  { name: "Aug-23", Scheduled: 0, Started: 0, Completed: 0, Closed: 0, Cancelled: 0, OverDue: 0 },
+  { name: "Sep-23", Scheduled: 0, Started: 0, Completed: 0, Closed: 0, Cancelled: 0, OverDue: 0 },
+  { name: "Oct-23", Scheduled: 0, Started: 0, Completed: 0, Closed: 0, Cancelled: 0, OverDue: 0 },
+  { name: "Nov-23", Scheduled: 0, Started: 0, Completed: 0, Closed: 0, Cancelled: 0, OverDue: 0 },
+  { name: "Dec-23", Scheduled: 0, Started: 0, Completed: 0, Closed: 0, Cancelled: 0, OverDue: 0 },
+  { name: "Jan-24", Scheduled: 0, Started: 0, Completed: 0, Closed: 0, Cancelled: 0, OverDue: 0 },
+  { name: "Feb-24", Scheduled: 0, Started: 0, Completed: 0, Closed: 0, Cancelled: 0, OverDue: 0 },
+  { name: "Mar-24", Scheduled: 0, Started: 0, Completed: 0, Closed: 0, Cancelled: 0, OverDue: 0 },
+  { name: "Apr-24", Scheduled: 0, Started: 0, Completed: 0, Closed: 0, Cancelled: 0, OverDue: 0 },
+  { name: "May-24", Scheduled: 0, Started: 0, Completed: 0, Closed: 0, Cancelled: 0, OverDue: 0 },
+  { name: "Jun-24", Scheduled: 1, Started: 0, Completed: 0, Closed: 0, Cancelled: 0, OverDue: 0 },
+  { name: "Jul-24", Scheduled: 6, Started: 0, Completed: 0, Closed: 0, Cancelled: 0, OverDue: 0 },
+];
+
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#8884d8", "#82ca9d", "#a4de6c", "#d0d1e6", "#f4cae4"];
 
 export default function MaintenanceDashboard() {
   return (
-    <div>
-      <Typography>Maintenance Dashboard</Typography>
-    </div>
+    <Box sx={{ width: "100%", height: "100%", display: "flex", flexDirection: "column" }}>
+      <Grid container sx={{ flex: 1 }}>
+        <Grid container item xs={12} sx={{ flex: 1 }}>
+
+          <Grid item xs={6} sx={{ height: "93%" }}>
+            <Item>
+              <PieChartMaintenance />
+            </Item>
+          </Grid>
+
+          <Grid item xs={6} sx={{ height: "93%" }}>
+            <Item>
+              <BarChartMaintenanceOverall />
+            </Item>
+          </Grid>
+        </Grid>
+
+        <Grid container item xs={12} sx={{ flex: 1 }}>
+
+          <Grid item xs={6} sx={{ height: "93%" }}>
+            <Item>
+              <BarChartMaintenanceYearly />
+            </Item>
+          </Grid>
+
+          <Grid item xs={6} sx={{ height: "93%" }}>
+            <Item>
+              <BarChartMaintenanceEachSys />
+            </Item>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
