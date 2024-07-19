50fdaee01201481026cf3e273761ca521f95452a
Justin Lim <justinlimchunkiat.work@gmail.com>
2024-07-19 12:15:15 +0800

    initial justin commit

---
+++ src/components/SideMenu/ListOfModuleItems.jsx
@@ -0,0 +1,12 @@
+export const LIST_OF_ITEMS = {
+  asset: {
+    Asset: [
+      "Manage Asset Type",
+      "Manage Asset Group",
+      "Manage Asset Category",
+      "Manage Asset",
+    ],
+    Point: ["Manage Point", "Manage Point Group"],
+    Reports: ["Asset Report", "Alarm Log Report", "Event Log Report"],
+  },
+};
